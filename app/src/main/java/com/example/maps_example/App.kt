044b1e2749e6e34ada5363b360d342281aeee75e
Daniel <daniel.danelyan@joy-dev.com>
2022-09-02 14:40:47 +0300

    DD: yandex map setup

--- app/src/main/java/com/example/maps_example/App.kt
+++ app/src/main/java/com/example/maps_example/App.kt
@@ -1,7 +1,9 @@
 package com.example.maps_example
 
 import android.app.Application
+import com.example.maps_example.BuildConfig.YANDEX_MAP_KIT_SDK_KEY
 import com.example.maps_example.di.viewModelModule
+import com.yandex.mapkit.MapKitFactory
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.android.x.androidXModule
@@ -11,5 +13,10 @@ class App : Application(), DIAware {
     override val di by DI.lazy {
         import(androidXModule(this@App))
         import(viewModelModule)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        MapKitFactory.setApiKey(YANDEX_MAP_KIT_SDK_KEY)
     }
 }
--- app/src/main/java/com/example/maps_example/ui/fragments/map/MapFragment.kt
+++ app/src/main/java/com/example/maps_example/ui/fragments/map/MapFragment.kt
@@ -5,11 +5,30 @@ import android.view.View
 import com.example.maps_example.R
 import com.example.maps_example.databinding.FragmentMapBinding
 import com.example.maps_example.ui.base.BaseFragment
+import com.yandex.mapkit.MapKitFactory
 import org.kodein.di.android.x.viewmodel.viewModel
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
 
     override val viewModel: MapVM by viewModel()
+    private val mapKit by lazy { MapKitFactory.getInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MapKitFactory.initialize(requireContext())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapKit.onStart()
+        binding?.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapKit.onStop()
+        binding?.mapView?.onStop()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
