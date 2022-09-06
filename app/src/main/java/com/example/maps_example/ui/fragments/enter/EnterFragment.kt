8c788527dd50f06cce38e94fb50b8ae5b68e4f8b
Daniel <daniel.danelyan@joy-dev.com>
2022-09-06 15:25:29 +0300

    DD: added location permission

--- app/src/main/java/com/example/maps_example/ui/fragments/enter/EnterFragment.kt
+++ app/src/main/java/com/example/maps_example/ui/fragments/enter/EnterFragment.kt
@@ -1,7 +1,11 @@
 package com.example.maps_example.ui.fragments.enter
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions
 import androidx.fragment.app.viewModels
 import com.example.maps_example.R
 import com.example.maps_example.databinding.FragmentEnterBinding
@@ -10,9 +14,16 @@ import com.example.maps_example.ui.base.BaseFragment
 class EnterFragment : BaseFragment<FragmentEnterBinding>(R.layout.fragment_enter) {
 
     override val viewModel by viewModels<EnterVM>()
+    private val permissions = arrayOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION)
+    private val locationPermission = registerForActivityResult(RequestMultiplePermissions()) {
+        if (it.all { permission -> permission.value }) {
+            Log.d("asdasd", "OK")
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.vm = viewModel
+        locationPermission.launch(permissions)
     }
 }
