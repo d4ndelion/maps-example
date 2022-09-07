2bc74314fc9ad8e07a04b1850dc4e68c6546c5a7
Daniel <daniel.danelyan@joy-dev.com>
2022-09-07 11:15:15 +0300

    DD: osm map setup

--- app/src/main/java/com/example/maps_example/ui/fragments/map/MapFragment.kt
+++ app/src/main/java/com/example/maps_example/ui/fragments/map/MapFragment.kt
@@ -7,10 +7,18 @@ import com.example.maps_example.databinding.FragmentMapBinding
 import com.example.maps_example.ui.base.BaseFragment
 import org.kodein.di.android.x.viewmodel.viewModel
 import org.osmdroid.config.Configuration.*
+import org.osmdroid.util.BoundingBox
+import org.osmdroid.views.CustomZoomButtonsController.Visibility.NEVER
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+
+private const val DEFAULT_ZOOM_LEVEL = 3.0
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
 
     override val viewModel: MapVM by viewModel()
+    private val locationOverlay by lazy { MyLocationNewOverlay(GpsMyLocationProvider(requireContext()), binding?.mapView) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +28,24 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.vm = viewModel
+        locationOverlay.enableMyLocation()
+        binding?.apply {
+            with(mapView) {
+                overlays.add(locationOverlay)
+                minZoomLevel = DEFAULT_ZOOM_LEVEL
+                isVerticalMapRepetitionEnabled = false
+                zoomController.setVisibility(NEVER)
+                setMultiTouchControls(true)
+                setScrollableAreaLimitLatitude(MapView.getTileSystem().maxLatitude, MapView.getTileSystem().minLatitude, 0)
+                mapView.zoomToBoundingBox(
+                    BoundingBox(
+                        MapView.getTileSystem().maxLatitude,
+                        MapView.getTileSystem().maxLongitude,
+                        MapView.getTileSystem().minLatitude,
+                        MapView.getTileSystem().minLongitude
+                    ), false
+                )
+            }
+        }
     }
 }
